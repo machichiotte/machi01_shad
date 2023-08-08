9d13b54325db866ca3e768e78d805946f23511da
machichiotte <machichiotte@gmail.com>
2023-08-08 10:20:25 -0300

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 // router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-//import HomePage from '@/components/HomePage';
+import HomePage from '@/components/HomePage';
 import Shad from '@/components/ShadPage';
 import ActiveOrders from '@/components/ActiveOrdersPage';
 import ShowData from '@/components/ShowDataPage';
@@ -9,7 +9,7 @@ import Strat from '@/components/StratPage';
 import Trades from '@/components/TradesPage';
 
 const routes = [
-  { path: '/', component: Shad },
+  { path: '/', component: Home },
   { path: '/update', component: Update },
   { path: '/active-orders', component: ActiveOrders },
   { path: '/show-data', component: ShowData },
