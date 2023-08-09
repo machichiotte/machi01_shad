03cdf9fdcc6b192322a257942338b4fae400368d
machichiotte <machichiotte@gmail.com>
2023-08-09 11:24:46 -0300

    Update server.js
    change addTradesManually path

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/cancel/order', deleteOrder);
 app.post('/cancel/all-orders', cancelAllOrders);
 app.post('/bunch-orders', createBunchOrders);
 
-app.post('/add/trade', addTrade);
+app.post('/add/trade', addTradesManually);
 
 //get
 async function getData(req, res, collection, mockDataFile) {
