5e774606edb32959cd56076608527eba51827cd8
machichiotte <machichiotte@gmail.com>
2024-07-01 22:15:37 -0500

    remove helmet call

--- src/services/requestHandlers.js
+++ src/services/requestHandlers.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const helmet = require("helmet"); // For added security
+//const helmet = require("helmet"); // For added security
 
 const app = express();
 
