1e42df3f90ebde81aa20e73cb0d30c2a3fa08563
machichiotte <machichiotte@gmail.com>
2024-07-06 17:28:42 -0500

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 // vite.config.js
 
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -13,6 +12,11 @@ export default defineConfig({
       '@comp': fileURLToPath(new URL('./src/components', import.meta.url)),
       '@btn': fileURLToPath(new URL('./src/components/buttons', import.meta.url)),
       '@form': fileURLToPath(new URL('./src/components/forms', import.meta.url)),
+      'vue$': 'vue/dist/vue.esm-bundler.js'
     }
+  },
+  build: {
+    outDir: 'dist', // Correspond à outputDir de Vue CLI
+    assetsDir: 'static' // Correspond à assetsDir de Vue CLI
   }
 })
