bacc3b4fce537d2f078a86bc688afd2c7a2fbef4
machichiotte <machichiotte@gmail.com>
2024-09-08 06:44:37 -0500

    add docstring to myemergencyselbutton.vue

--- src/components/buttons/MyEmergencySellButton.vue
+++ src/components/buttons/MyEmergencySellButton.vue
@@ -5,16 +5,19 @@
 
 <script setup>
 import { marketSellOrder, cancelAllSellOrders } from '../../js/orders.js'
-import { loadingSpin, successSpinHtml, errorSpin } from '../../js/spinner.js'
+import { loadingSpin, successSpinHtml } from '../../js/spinner.js'
 
 const props = defineProps({
   selectedAssets: Object // Prop pour les assets sélectionnés
 })
 
+/**
+ * @async
+ * @function emergencySellClicked
+ * @returns {Promise<void>}
+ */
 const emergencySellClicked = async () => {
-  console.log(`🚀 ~ file: MyEmergencySellButton.vue:15 ~ emergencySellClicked ~ async:`)
   const selectedRows = props.selectedAssets;
-  console.log(`🚀 ~ file: MyEmergencySellButton.vue:17 ~ emergencySellClicked ~ selectedRows:`, selectedRows)
 
   loadingSpin();
 
@@ -28,20 +31,14 @@ const emergencySellClicked = async () => {
 
   const cancellationResults = await Promise.all(cancellationPromises);
 
-  console.log(`🚀 ~ file: MyEmergencySellButton.vue:15 ~ emergencySellClicked ~ cancel:`)
-
   // Filtre les annulations réussies et extrait les actifs
   const assetsToPlaceOrders = cancellationResults.filter(Boolean);
 
   if (assetsToPlaceOrders.length > 0) {
     const orderPlacementPromises = assetsToPlaceOrders.map(async (asset) => {
       const selectedRow = selectedRows.find((row) => row.asset === asset);
-      console.log(`🚀 ~ file: MyEmergencySellButton.vue:39 ~ orderPlacementPromises ~ selectedRow:`, selectedRow)
       const balance = selectedRow.balance; // Montant total à vendre au prix du marché
-      console.log(`🚀 ~ file: MyEmergencySellButton.vue:41 ~ orderPlacementPromises ~ balance:`, balance)
-
       const orderResult = await marketSellOrder(selectedRow.platform, asset, balance);
-      console.log(`🚀 ~ file: MyEmergencySellButton.vue:44 ~ orderPlacementPromises ~ orderResult:`, orderResult)
 
       return `${asset}: ${orderResult}`; // Résultat pour le journal
     });
