ec418ed3fee35d2cc2ad301d49582f16035fb59b
machichiotte <machichiotte@gmail.com>
2024-09-08 07:16:17 -0500

    add doctring for file upload middleware

--- src/middlewares/fileUploadMiddleware.js
+++ src/middlewares/fileUploadMiddleware.js
@@ -1,6 +1,18 @@
 // src/middlewares/fileUploadMiddleware.js
 
-// Middleware function to handle file uploads
+/**
+ * Middleware function to handle file uploads
+ * 
+ * This middleware checks if a file has been uploaded with the request.
+ * If a file is present, it sets the file in the request object for further processing.
+ * If no file is uploaded, it returns a 400 Bad Request response.
+ * In case of any errors, it returns a 500 Internal Server Error response.
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ * @returns {void}
+ */
 function fileUploadMiddleware(req, res, next) {
   try {
     // Check if a file has been uploaded
