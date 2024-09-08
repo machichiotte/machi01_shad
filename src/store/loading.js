d44cab536422456b9ec8e2c8c9b959475389490d
machichiotte <machichiotte@gmail.com>
2024-09-08 06:23:57 -0500

    add docstring to loading.js

--- src/store/loading.js
+++ src/store/loading.js
@@ -1,12 +1,25 @@
-// src/store/loading.js
-import { defineStore } from 'pinia';
+/**
+ * @typedef {Object} LoadingState
+ * @property {boolean} showLoading
+ */
 
+/**
+ * @typedef {Object} LoadingActions
+ * @property {function(boolean): void} setLoading
+ */
+
+/**
+ * @type {import('pinia').StoreDefinition<'loading', LoadingState, {}, LoadingActions>}
+ */
 export const useLoadingStore = defineStore('loading', {
   state: () => ({
     showLoading: false,
   }),
 
   actions: {
+    /**
+     * @param {boolean} payload
+     */
     setLoading(payload) {
       this.showLoading = payload;
     },
