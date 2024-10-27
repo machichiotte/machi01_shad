683ca550a8b4f9a95d4f8c9f7bfaf2b60375bcb4
machichiotte <machichiotte@gmail.com>
2024-10-27 09:47:22 -0500

    improve design search bar

--- src/components/machi/SearchBar.vue
+++ src/components/machi/SearchBar.vue
@@ -1,17 +1,11 @@
 <!-- src/components/machi/SearchBar.vue -->
 <template>
-    <div class="search-container mb-4">
-        <Toolbar>
-            <template #start>
-                <IconField iconPosition="left">
-                    <InputIcon>
-                        <i class="pi pi-search" />
-                    </InputIcon>
-                    <InputText v-model="props.filters.global.value" placeholder="Search..." />
-                </IconField>
-            </template>
-        </Toolbar>
-    </div>
+    <IconField iconPosition="left">
+        <InputIcon>
+            <i class="pi pi-search" />
+        </InputIcon>
+        <InputText v-model="props.filters.global.value" placeholder="Search..." />
+    </IconField>
 </template>
 
 <script setup lang="ts">
@@ -26,9 +20,3 @@ const props = defineProps<{
     filters: Filters;
 }>();
 </script>
-
-<style scoped>
-.search-container {
-    margin-bottom: 1rem;
-}
-</style>
