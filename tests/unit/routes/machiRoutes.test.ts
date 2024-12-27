fc7c1a75a5c7bcf0764babb0dc729ec50da7c341
machichiotte <machichiotte@gmail.com>
2024-12-26 20:56:06 -0500

    modify test file name

--- tests/unit/routes/machiRoutes.test.ts
+++ tests/unit/routes/machiRoutes.test.ts
@@ -3,38 +3,38 @@ import request from 'supertest';
 import machiRoutes from '../../../src/routes/machiRoutes';
 import * as machiController from '../../../src/controllers/machiController';
 
-// Mock du contrôleur Shad
+// Mock du contrôleur Machi
 jest.mock('../../../src/controllers/machiController');
 
-describe('shadRoutes', () => {
+describe('machiRoutes', () => {
     let app: express.Application;
 
     beforeEach(() => {
         app = express();
-        app.use('/shad', machiRoutes);
+        app.use('/machi', machiRoutes);
     });
 
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    describe('GET /shad/get', () => {
-        it('devrait appeler getShad et renvoyer 200', async () => {
-            const mockGetShad = machiController.getMachi as jest.MockedFunction<typeof machiController.getMachi>;
-            mockGetShad.mockImplementation(async (req, res) => {
-                res.status(200).json({ message: 'Données Shad récupérées' });
+    describe('GET /machi/get', () => {
+        it('devrait appeler getMachi et renvoyer 200', async () => {
+            const mockGetMachi = machiController.getMachi as jest.MockedFunction<typeof machiController.getMachi>;
+            mockGetMachi.mockImplementation(async (req, res) => {
+                res.status(200).json({ message: 'Données Machi récupérées' });
                 return Promise.resolve();
             });
 
-            const response = await request(app).get('/shad/get');
+            const response = await request(app).get('/machi/get');
 
             expect(response.status).toBe(200);
-            expect(mockGetShad).toHaveBeenCalled();
-            expect(response.body).toEqual({ message: 'Données Shad récupérées' });
+            expect(mockGetMachi).toHaveBeenCalled();
+            expect(response.body).toEqual({ message: 'Données Machi récupérées' });
         });
     });
 
-    describe('POST /shad/trailing', () => {
+    describe('POST /machi/trailing', () => {
         it('devrait appeler handleTrailingStopHedge et renvoyer 200', async () => {
             const mockHandleTrailingStopHedge = machiController.handleTrailingStopHedge as jest.MockedFunction<typeof machiController.handleTrailingStopHedge>;
             mockHandleTrailingStopHedge.mockImplementation(async (req, res) => {
@@ -43,7 +43,7 @@ describe('shadRoutes', () => {
             });
 
             const response = await request(app)
-                .post('/shad/trailing')
+                .post('/machi/trailing')
                 .send({ /* Ajoutez ici les données nécessaires pour le test */ });
 
             expect(response.status).toBe(200);
