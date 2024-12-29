ba2df26d1539ad9e4284acd828462445715d2ffb
machichiotte <machichiotte@gmail.com>
2024-12-29 12:32:59 -0500

    modif config route

---
+++ src/routes/config/apiRoutes.ts
@@ -0,0 +1,12 @@
+// src/routes/config/apiRoutes.ts
+import express from 'express'
+import { getApiConfig, updateApiConfig, updateApiKey } from '@src/controllers/config/apiConfigController'
+
+const router = express.Router()
+
+router.get('/get', getApiConfig)
+router.post('/update', updateApiConfig)
+router.post('/update/key', updateApiKey)
+
+
+export default router
