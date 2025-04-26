8f2bb08ec6762d7cf2a11c641b5c0267c9eecf4c
machichiotte <machichiotte@gmail.com>
2025-04-26 12:18:57 -0500

    fix config

--- src/server/config.ts
+++ src/server/config.ts
@@ -4,11 +4,11 @@ import { ApiResponse } from '../types/response'
 import { ApiConfig, KeyPayload } from '../types/config';
 
 const updateApiConfig = async <T>(config: ApiConfig): Promise<ApiResponse<T>> => {
-  return executeApiRequest('/config/api/update', { config })
+  return executeApiRequest('/config_api/update', { config })
 }
 
 const updateKey = async <T>(payload: KeyPayload): Promise<ApiResponse<T>> => {
-  return pleaseApiUpdate('/config/api/update/key', payload);
+  return pleaseApiUpdate('/config_api/update/key', payload);
 };
 
 export { updateApiConfig, updateKey }
